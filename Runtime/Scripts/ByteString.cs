8b3687c77643e03a45a8a3fd230b8cb91204221a
Stella Cannefax <stella@unity3d.com>
2019-09-24 23:35:58 -0700

    initial commit

---
+++ Runtime/Scripts/ByteString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace ByteStrings
+{
+    public struct ByteString : IDisposable
+    {
+        public NativeArray<byte> Bytes;
+
+        public ByteString(string source, Allocator allocator = Allocator.Persistent)
+        {
+            var bytes = Encoding.UTF8.GetBytes(source);
+            Bytes = new NativeArray<byte>(bytes, allocator);
+        }
+
+        public override string ToString()
+        {
+            unsafe
+            {
+                var ptr = (byte*) Bytes.GetUnsafeReadOnlyPtr();
+                return Encoding.UTF8.GetString(ptr, Bytes.Length);
+            }
+        }
+        
+        public void Dispose()
+        {
+            if(Bytes.IsCreated) Bytes.Dispose();
+        }
+    }
+}
+
---
+++ Tests/Editor/BasicTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Unity.Collections;
+
+namespace ByteStrings.Tests
+{
+    public class BasicTests
+    {
+        ByteString m_String;
+
+        [TearDown]
+        public void AfterEach()
+        {
+            m_String.Dispose();
+        }
+
+        [TestCase("Eat the rich")]
+        [TestCase("Medicare for all")]
+        [TestCase("Health justice now!")]
+        public void ToString_OutputIsIdentical(string input)
+        {
+            m_String = new ByteString(input, Allocator.Temp);
+            Assert.AreEqual(input, m_String.ToString());
+        }
+    }
+}
